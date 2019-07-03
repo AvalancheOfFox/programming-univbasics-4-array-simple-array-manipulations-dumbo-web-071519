cff5ad645eeb0686c6253c5504888e35b7953074
Ross Perkel <rossperkel@gmail.com>
2019-07-03 20:23:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,4 +22,23 @@ def shift_with_args(array)
   array.shift(2)
 end
 
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, num)
+  array.delete[num]
+end
 
